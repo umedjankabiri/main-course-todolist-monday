1d8d62c6d8a7a9af5e1251f997bbbe7f02ee6a89
umedjankabiri <umedjan.kabiri@gmail.com>
2024-06-21 16:39:53 +0300

    changed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import './App.css';
+import './App.css'
+import {Todolist} from "common/components/Todolist.tsx";
+import './App.css'
 
 function App() {
    return (
       <div className="App">
-         <div>
-            <h3>What to learn</h3>
-            <div>
-               <input/>
-               <button>+</button>
-            </div>
-            <ul>
-               <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-               <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-               <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
-            <div>
-               <button>All</button>
-               <button>Active</button>
-               <button>Completed</button>
-            </div>
-         </div>
+         <Todolist />
+         <Todolist />
+         <Todolist />
       </div>
-   );
+   )
 }
 
-export default App;
+export default App
