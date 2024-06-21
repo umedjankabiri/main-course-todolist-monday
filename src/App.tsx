e30b9ade0139bbceaa403f18f903ae840bc23ead
umedjankabiri <umedjan.kabiri@gmail.com>
2024-06-21 17:10:25 +0300

    add Tasks types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
-import {Todolist} from "./common/components/Todolist.tsx";
+import {Todolist} from "./common/components/Todolist";
 
 function App() {
+   const tasks1 = [
+      {id: 1, title: 'HTML&CSS', isDone: true},
+      {id: 2, title: 'JS', isDone: true},
+      {id: 3, title: 'ReactJS', isDone: false},
+   ]
+
+   const tasks2 = [
+      {id: 1, title: 'Hello world', isDone: true},
+      {id: 2, title: 'I am Happy', isDone: false},
+      {id: 3, title: 'Yo', isDone: false},
+   ]
+
    return (
       <div className="App">
-         <Todolist title = "What to learn" />
-         <Todolist title = "Songs" />
-         <Todolist title = "Books" />
+         <Todolist
+            title="What to learn"
+            tasks={tasks1}
+            date={'21.06.2024'}
+         />
+         <Todolist
+            title="Songs"
+            tasks={tasks2}
+         />
       </div>
    )
 }
--- src/common/types/TodolistProps.ts
+++ src/common/types/TodolistProps.ts
@@ -1,3 +1,10 @@
 export type TodolistProps = {
    title: string
+   tasks: TaskProps[]
+   date?: string
+}
+export type TaskProps = {
+   id: number
+   title: string
+   isDone: boolean
 }
