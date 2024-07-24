dbacd1df37a9c138716a413cb48cca019344c750
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-24 13:30:11 +0300

    changed path of import. Implemented hooks, added removeTask changeFilter functions

---
+++ src/common/components/App/App.tsx
@@ -0,0 +1,42 @@
+import 'common/components/App/App.css'
+import {Todolist} from "common/components/Todolist/Todolist.tsx";
+import {TasksProps} from "common/types/tasksProps/TasksProps.ts";
+import {useState} from "react";
+import {FilterValuesProps} from "common/types/todolistProps/FilterValuesProps.ts";
+
+let initialState: TasksProps[] = [
+   {id: 1, title: 'HTML&CSS', isDone: true},
+   {id: 2, title: 'JS', isDone: true},
+   {id: 3, title: 'ReactJS', isDone: false},
+   {id: 4, title: 'Redux', isDone: false},
+   {id: 5, title: 'TypeScript', isDone: false},
+   {id: 6, title: 'RTK Query', isDone: false},
+]
+
+function App() {
+   const [tasks, setTasks] = useState<TasksProps[]>(initialState);
+   const [filteredTasks, setFilteredTasks] = useState<FilterValuesProps>("All");
+
+   let tasksForTodolist = tasks
+   filteredTasks === "Active" ? tasksForTodolist = tasks.filter(task => !task.isDone) : tasksForTodolist
+   filteredTasks === "Completed" ? tasksForTodolist = tasks.filter(task => task.isDone) : tasksForTodolist
+
+   const removeTask = (taskID: number) => {
+      const filteredTasks = tasks.filter(task => task.id !== taskID)
+      setTasks(filteredTasks);
+   }
+   const changedFilter = (filter: FilterValuesProps) => setFilteredTasks(filter)
+
+   return (
+      <div className="App">
+         <Todolist
+             title="What to learn"
+             tasks={tasksForTodolist}
+             removeTask={removeTask}
+             changeFilter={changedFilter}
+         />
+      </div>
+   )
+}
+
+export default App
