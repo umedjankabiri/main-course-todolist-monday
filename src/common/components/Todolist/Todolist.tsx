9b8dbf438125490bcb3690c046dfb7344ba316d7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-24 13:41:23 +0300

    added Button component

--- src/common/components/Todolist/Todolist.tsx
+++ src/common/components/Todolist/Todolist.tsx
@@ -1,24 +1,26 @@
-import {TodolistProps} from "common/types/TodolistProps";
-import {Button} from "common/components/Button.tsx";
+import {TodolistProps} from "common/types/todolistProps/TodolistProps.ts";
+import {Button} from "common/components/Button/Button.tsx";
 import {FC} from "react";
 
 export const Todolist: FC<TodolistProps> = (props) => {
    const mappedTasks = props.tasks.map(task => {
       return (
          <li key={task.id}>
-            <input type="checkbox" checked={task.isDone} />
+            <input type="checkbox" checked={task.isDone} onChange={()=> {}}/>
             <span>{task.title}</span>
+            <Button title= 'x' onClick={()=> props.removeTask(task.id)}/>
          </li>
       )
    })
 
    return (
+
       <>
          <div>
             <h3>{props.title}</h3>
             <div>
                <input/>
-               <button>+</button>
+               <Button title = '+' />
             </div>
             <ul>
                {
@@ -26,11 +28,10 @@ export const Todolist: FC<TodolistProps> = (props) => {
                }
             </ul>
             <div>
-               <Button title= "All " />
-               <Button title= "Active"/>
-               <Button title= "Completed"/>
+               <Button title= "All " onClick={()=> props.changeFilter("All")}/>
+               <Button title= "Active" onClick={()=> props.changeFilter("Active")}/>
+                <Button title= "Completed" onClick={()=> props.changeFilter("Completed")}/>
             </div>
-            <div>{props.date}</div>
          </div>
       </>
    )
