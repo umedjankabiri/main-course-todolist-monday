4d1645cc566c69e697e103db641f42566ad6b517
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-24 13:45:11 +0300

    added Button props

--- src/common/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import {ButtonProps} from "common/types/ButtonProps";
-
-export const Button = (props: ButtonProps) => {
-   return (
-      <>
-         <button>{props.title}</button>
-      </>
-   )
-}
