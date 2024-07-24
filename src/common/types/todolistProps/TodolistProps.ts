3afa3c076a6a5d65090ab4101f458705a7f9dd53
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-24 15:05:13 +0300

    renamed path of FilterValueProps

--- src/common/types/todolistProps/TodolistProps.ts
+++ src/common/types/todolistProps/TodolistProps.ts
@@ -1,9 +1,9 @@
 import {TaskProps} from "common/types/tasksProps/TaskProps.ts";
-import {FilterValuesProps} from "common/types/todolistProps/FilterValuesProps.ts";
+import {FilterValueProps} from "common/types/todolistProps/FilterValueProps.ts";
 
 export type TodolistProps = {
    title: string
    tasks: TaskProps[]
    removeTask: (taskID: number) => void
-   changeFilter: (filtered: FilterValuesProps) => void
+   changeFilter: (filtered: FilterValueProps) => void
 }
