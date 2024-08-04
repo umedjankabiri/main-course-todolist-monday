0fc17a919d8755e8aa9b521947d1c74a1dc0665a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-05 02:01:39 +0300

    Enhanced Todolist component, added changeTaskStatusHandler callback function for changing status of task. Implemented ternary operation, for changeTaskTitleHandler callback function. Added new useState for monitoring condition errors, added classNames for styles

--- src/common/components/Todolist/Todolist.tsx
+++ src/common/components/Todolist/Todolist.tsx
@@ -4,23 +4,34 @@ import {ChangeEvent, FC, useState, KeyboardEvent} from "react";
 
 export const Todolist: FC<TodolistProps> = (props) => {
     const [taskTitle, setTaskTitle] = useState("")
+    const [error, setError] = useState<string | null>(null)
 
-    const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) => setTaskTitle(event.currentTarget.value)
+    const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) =>
+        setTaskTitle(event.currentTarget.value)
     const addTaskHandler = () => {
-        props.addTask(taskTitle);
-        setTaskTitle("")
+        taskTitle.trim() !== ''
+            ? (props.addTask(taskTitle.trim()), setTaskTitle(""))
+            : setError("Title is required");
     }
-    const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => (event.ctrlKey && event.key === "Enter") && addTaskHandler()
+    const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        taskTitle.trim() !== '' && setError(null);
+        (event.ctrlKey && event.key === "Enter") && addTaskHandler()
+    }
+
     const setAllTasksHandler = () => props.changeFilter("All")
     const setActiveTasksHandler = () => props.changeFilter("Active")
     const setCompletedTasksHandler = () => props.changeFilter("Completed")
 
     const mappedTasks = props.tasks.map(task => {
         const removeTaskHandler = () => props.removeTask(task.id)
+        const changeTaskStatusHandler = (event: ChangeEvent<HTMLInputElement>) =>
+            props.changeTaskStatus(task.id, event.currentTarget.checked)
+
         return (
-            <li key={task.id}>
-                <input type="checkbox" checked={task.isDone} onChange={() => {
-                }}/>
+            <li className={task.isDone ? 'isDone' : ''} key={task.id}>
+                <input className={error ? 'error' : ''}
+                       type="checkbox" checked={task.isDone}
+                       onChange={changeTaskStatusHandler}/>
                 <span>{task.title}</span>
                 <Button title='x' onClick={removeTaskHandler}/>
             </li>
@@ -32,8 +43,11 @@ export const Todolist: FC<TodolistProps> = (props) => {
             <div>
                 <h3>{props.title}</h3>
                 <div>
-                    <input value={taskTitle} onChange={changeTaskTitleHandler} onKeyUp={addTaskOnKeyUpHandler}/>
+                    <input value={taskTitle}
+                           onChange={changeTaskTitleHandler}
+                           onKeyUp={addTaskOnKeyUpHandler}/>
                     <Button title='+' onClick={addTaskHandler}/>
+                    {error && <div className={'errorMessage'}>{error}</div>}
                 </div>
                 <ul>
                     {
@@ -41,9 +55,9 @@ export const Todolist: FC<TodolistProps> = (props) => {
                     }
                 </ul>
                 <div>
-                    <Button title="All " onClick={setAllTasksHandler}/>
-                    <Button title="Active" onClick={setActiveTasksHandler}/>
-                    <Button title="Completed" onClick={setCompletedTasksHandler}/>
+                    <Button className={props.filter === 'All' ? 'activeFilter' : ''} title="All " onClick={setAllTasksHandler}/>
+                    <Button className={props.filter === 'Active' ? 'activeFilter' : ''} title="Active" onClick={setActiveTasksHandler}/>
+                    <Button className={props.filter === 'Completed' ? 'activeFilter' : ''} title="Completed" onClick={setCompletedTasksHandler}/>
                 </div>
             </div>
         </>
