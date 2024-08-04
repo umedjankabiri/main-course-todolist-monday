e7996f918e400d13bc03cc0cea80e27654098ea0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-05 02:03:40 +0300

    Implemented classNames property for button component

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import {ButtonProps} from "common/types/buttonProps/ButtonProps.ts";
 export const Button = (props: ButtonProps) => {
    return (
       <>
-         <button onClick={props.onClick}>{props.title}</button>
+         <button className={props.className} onClick={props.onClick}>{props.title}</button>
       </>
    )
 }
